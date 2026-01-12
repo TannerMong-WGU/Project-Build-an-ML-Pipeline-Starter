6aa644e8521270f4412b5f8e20469c7f407fd229
Tanner Mong <tmong1@wgu.edu>
2026-01-12 17:15:45 -0500

    Fixes bug with main.py basic cleaning implementation

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def go(config: DictConfig):
                 entry_point="main",
                 env_manager="conda",
                 parameters={
-                    "input_artifact": config["etl"]["sample"],
+                    "input_artifact": f"{config['etl']['sample']}:latest",
                     "output_artifact": "clean_sample.csv",
                     "output_type": "cleaned_data",
                     "output_description": "Basic cleaned version of the Airbnb dataset",
